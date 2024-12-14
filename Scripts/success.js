95432dd84f30759949cf16f889714a1fb5ce50f0
SuthanK10 <suthankuruvayooran10@gmail.com>
2024-12-14 23:13:46 +0530

    Update success.js

--- Scripts/success.js
+++ Scripts/success.js
@@ -1,1 +1,9 @@
+        const deliveryMessage = document.getElementById('delivery-message');
+        const deliveryDate = new Date();
+        deliveryDate.setDate(deliveryDate.getDate() + 5); // Add 5 days
 
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const formattedDate = deliveryDate.toLocaleDateString('en-US', options); // Format the date
+
+        // Update the delivery message with the calculated date
+        deliveryMessage.textContent = `Thank you for your purchase. Your order will be delivered on ${formattedDate}.`;
